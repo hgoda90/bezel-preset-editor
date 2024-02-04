c38c82039c905b32909ee7d2070c4ec094017799
hgoda90 <hgoda90@gmail.com>
2024-02-04 15:40:06 -0500

    Add files via upload

--- color.php
+++ color.php
@@ -1,4 +1,5 @@
 <?php
+
 $HEX = "808080";
 $contrast = "1.30";
 $HEXError = $contrastError = "";
@@ -35,76 +36,7 @@ function check_hex($color) {
 	}
 }
 
-echo '<html>
-	<style>
-		body{
-			font-family: monospace;
-			font-size: 16px;
-			margin: 16px;
-			width: 410px;
-			height: 410px;
-		}
-		
-		.hex, .contrast{
-			display: inline-block;
-			margin-top: 14px;
-		}
-		
-		.hex input{
-			text-transform:uppercase;
-		}
-		
-		.square{
-			border: 2px solid #000;
-			background: #'.$HEX.';
-			height: 72px;
-			width: 84px;
-			float: right;
-			margin-top: -40px;
-		}
-		
-		.error {
-			color: #FF0000;
-			display: inline-block;
-			margin-top: 4px;
-		}
-		
-		#submit{
-			margin-top: 24px;
-			width: 20%;
-		}
-		
-		#submit:hover{
-			cursor: pointer;
-		}
-		
-	</style>
-	<body>
-		<h2 id="title">HEX to koko-aio Color Converter</h2>
-		 <form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'" autocomplete="off">';
-		 
-		 if($HEX == "808080"){
-			echo 'BEZEL_R = "0.000000"<br>
-			BEZEL_G = "0.000000"<br>
-			BEZEL_B = "0.000000"<br>
-			BEZEL_CON = "'.$contrast.'0000"<br><br>';
-		 }
-		 else{
-			 echo 'BEZEL_R = "'.number_format((float)floatval(($R - 128) * ((0.395 - ($contrast - 1.30) *  (((3.75 - $contrast) * .125) + .00125)) / 128)), 6, '.', '').'"<br>
-				BEZEL_G = "'.number_format((float)floatval(($G - 128) * ((0.395 - ($contrast - 1.30) *  (((3.75 - $contrast) * .125) + .00125)) / 128)), 6, '.', '').'"<br>
-				BEZEL_B = "'.number_format((float)floatval(($B - 128) * ((0.395 - ($contrast - 1.30) *  (((3.75 - $contrast) * .125) + .00125)) / 128)), 6, '.', '').'"<br>
-				BEZEL_CON = "'.$contrast.'0000"<br><br>';
-		 }
-			
-			echo '<div class="hex">HEX: # <input type="text" name="hex" value="'.$HEX.'"></div>
-			<div class="contrast">Contrast: <input type="text" name="contrast" value="'.$contrast.'"></div>
-			<div class="square"></div>
-			<span class="error">'.$HEXError.'</span>
-			<span class="error">'.$contrastError.'</span>
-			<br>
-			<br>
-			<input type="submit" name="submit" value="Convert" id="submit">
-		</form>
-	</body>
-</html>';
+$content = array($R, $G, $B, $contrast);
+
+echo json_encode($content);
 ?>
