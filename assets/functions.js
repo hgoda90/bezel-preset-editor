943a575fb538506061895e599df8947b8a3888b3
hgoda90 <hgoda90@gmail.com>
2024-02-07 07:52:55 -0500

    add Mega Bezel conversion and change layout

--- assets/functions.js
+++ assets/functions.js
@@ -24,32 +24,114 @@ function getCookie(cname) {
 	return "";
 }
 
+function bezelToggle(){
+	$(".colors").empty();
+	
+	if($(".switch-panel.bezel input").is(":checked")){
+		setCookie("bezelStyle", "mbz", 30);
+		bezelStyle = "mbz";
+		$(".contrast").css("opacity", 0);
+		
+		$(".square").css("background", "#1A1A1A");
+		$(".square").css("box-shadow", "0px 0px 14px 4px rgba(26, 26, 26, 0.25)");
+		$(".conversion").text('HSM_BZL_COLOR_HUE = "0.000000"\nHSM_BZL_COLOR_SATURATION = "0.000000"\nHSM_BZL_COLOR_VALUE = "10.000000"');
+		
+		if($("div").hasClass("hex") == true){
+			$(".hex input").attr("placeholder", "1A1A1A");
+		}
+		else{
+			$(".rgb input[name='red']").attr("placeholder", "26");
+			$(".rgb input[name='green']").attr("placeholder", "26");
+			$(".rgb input[name='blue']").attr("placeholder", "26");
+		}
+	}
+	else{
+		setCookie("bezelStyle", "koko-aio", 30);
+		bezelStyle = "koko-aio";
+		$(".contrast").css("opacity", 1);
+		$(".contrast option").prop("selected", false);
+		$(".contrast option[value='1.30']").prop("selected", "selected");
+		
+		$(".square").css("background", "#808080");
+		$(".square").css("box-shadow", "0px 0px 14px 4px rgba(128, 128, 128, 0.25)");
+		$(".conversion").text('BEZEL_R = "0.000000"\nBEZEL_G = "0.000000"\nBEZEL_B = "0.000000"\nBEZEL_CON = "1.300000"');
+	
+		if($("div").hasClass("hex") == true){
+			$(".hex input").attr("placeholder", "808080");
+		}
+		else{
+			$(".rgb input[name='red']").attr("placeholder", "128");
+			$(".rgb input[name='green']").attr("placeholder", "128");
+			$(".rgb input[name='blue']").attr("placeholder", "128");
+		}
+	}
+	
+	$(".info").empty();
+	swatches();
+};
+
 function formatToggle(){
 	$(".colors").empty();
 	
-	if($(".switch input").is(":checked")){
+	if($(".switch-panel.format input").is(":checked")){
 		setCookie("colorFormat", "RGB", 30);
 		colorFormat = "RGB";
 		$(".hex").remove();
-		$("textarea").after('<div class="rgb">rgb(<input type="text" name="red" placeholder="128">, <input type="text" name="green" placeholder="128">, <input type="text" name="blue" placeholder="128">)</div>');
-		$(".square").css("background", "#808080");
+		
+		if(bezelStyle == "koko-aio"){
+			$("textarea").after('<div class="rgb">rgb(<input type="text" name="red" placeholder="128">, <input type="text" name="green" placeholder="128">, <input type="text" name="blue" placeholder="128">)</div>');
+			$(".square").css("background", "#808080");
+			$(".square").css("box-shadow", "0px 0px 14px 4px rgba(128, 128, 128, 0.25)");
+			$(".conversion").text('BEZEL_R = "0.000000"\nBEZEL_G = "0.000000"\nBEZEL_B = "0.000000"\nBEZEL_CON = "1.300000"');
+			$(".contrast").css("opacity", 1);
+			$(".contrast option").prop("selected", false);
+			$(".contrast option[value='1.30']").prop("selected", "selected");
+		}
+		else{
+			$("textarea").after('<div class="rgb">rgb(<input type="text" name="red" placeholder="26">, <input type="text" name="green" placeholder="26">, <input type="text" name="blue" placeholder="26">)</div>');
+			$(".square").css("background", "#1A1A1A");
+			$(".square").css("box-shadow", "0px 0px 14px 4px rgba(26, 26, 26, 0.25)");
+			$(".conversion").text('HSM_BZL_COLOR_HUE = "0.000000"\nHSM_BZL_COLOR_SATURATION = "0.000000"\nHSM_BZL_COLOR_VALUE = "10.000000"');
+			$(".contrast").css("opacity", 0);
+		}
 	}
 	else{
 		setCookie("colorFormat", "HEX", 30);
 		colorFormat = "HEX";
 		$(".rgb").remove();
-		$("textarea").after('<div class="hex">HEX: # <input type="text" name="hex" placeholder="808080"></div>');
-		$(".square").css("background", "rgb(128, 128, 128)");
+		
+		if(bezelStyle == "koko-aio"){
+			$("textarea").after('<div class="hex">HEX: # <input type="text" name="hex" placeholder="808080"></div>');
+			$(".square").css("background", "#808080");
+			$(".square").css("box-shadow", "0px 0px 14px 4px rgba(128, 128, 128, 0.25)");
+			$(".conversion").text('BEZEL_R = "0.000000"\nBEZEL_G = "0.000000"\nBEZEL_B = "0.000000"\nBEZEL_CON = "1.300000"');
+			$(".contrast").css("opacity", 1);
+			$(".contrast option").prop("selected", false);
+			$(".contrast option[value='1.30']").prop("selected", "selected");
+		}
+		else{
+			$("textarea").after('<div class="hex">HEX: # <input type="text" name="hex" placeholder="1A1A1A"></div>');
+			$(".square").css("background", "#1A1A1A");
+			$(".square").css("box-shadow", "0px 0px 14px 4px rgba(26, 26, 26, 0.25)");
+			$(".conversion").text('HSM_BZL_COLOR_HUE = "0.000000"\nHSM_BZL_COLOR_SATURATION = "0.000000"\nHSM_BZL_COLOR_VALUE = "10.000000"');
+			$(".contrast").css("opacity", 0);
+		}
 	}
 	
-	$(".square").css("box-shadow", "0px 0px 14px 4px rgba(128, 128, 128, 0.25)");
-	$(".conversion").text('BEZEL_R = "0.000000"\nBEZEL_G = "0.000000"\nBEZEL_B = "0.000000"\nBEZEL_CON = "1.300000"');
 	$(".info").empty();
 	swatches();
 }
 
+function rgbToHSB(r, g, b){
+    r /= 255, g /= 255, b /= 255;
+	var v = Math.max(r, g, b), n = v - Math.min(r, g, b);
+	var h = (n === 0) ? 0 : n && v === r ? (g - b) / n : v === g ? 2 + (b - r) / n : 4 + (r - g) / n;
+	
+    return [60 * (h < 0 ? h + 6 : h), v && (n / v) * 100, v * 100];
+}
+
 function swatches(){
-	if($(".switch input").is(":checked")){
+	if($(".switch-panel.format input").is(":checked")){
 		$(".swatch").each(function(element){
 			switch($(this).attr("id")){
 				case "a26":
@@ -82,18 +164,20 @@ function swatches(){
 				case "snes":
 					$(this).find(".colors").append("<div class='color' data-code='rgb(181, 182, 228)' style='background: rgb(181, 182, 228);'></div>");
 					$(this).find(".colors").append("<div class='color' data-code='rgb(85, 58, 153)' style='background: rgb(85, 58, 153);'></div>");
+					$(this).find(".colors").append("<div class='color' data-code='rgb(144,138,153)' style='background: rgb(144,138,153);'></div>");
 					$(this).find(".colors").append("<div class='color' data-code='rgb(209, 209, 209)' style='background: rgb(209, 209, 209);'></div>");
 					break;
 			}
 		});
+		$(".color-code").css("text-align", "center");
 	}
 	else{
 		$(".swatch").each(function(){
 			switch($(this).attr("id")){
 				case "a26":
 					$(this).find(".colors").append("<div class='color' data-code='#242323' style='background: #242323;'></div>");
-					$(this).find(".color").append("<div class='color' data-code='#946E4A' style='background: #946E4A'></div>");
-					$(this).find(".color").append("<div class='color' data-code='#FF9233' style='background: #FF9233;'></div>");
+					$(this).find(".colors").append("<div class='color' data-code='#946E4A' style='background: #946E4A'></div>");
+					$(this).find(".colors").append("<div class='color' data-code='#FF9233' style='background: #FF9233;'></div>");
 					break;
 				case "dc":
 					$(this).find(".colors").append("<div class='color' data-code='#ED8332' style='background: #ED8332;'></div>");
@@ -120,11 +204,17 @@ function swatches(){
 				case "snes":
 					$(this).find(".colors").append("<div class='color' data-code='#B5B6E4' style='background: #B5B6E4;'></div>");
 					$(this).find(".colors").append("<div class='color' data-code='#553A99' style='background: #553A99;'></div>");
+					$(this).find(".colors").append("<div class='color' data-code='#908A99' style='background: #908A99;'></div>");
 					$(this).find(".colors").append("<div class='color' data-code='#D1D1D1' style='background: #D1D1D1;'></div>");
 					break;
 			}
 		});
+		
+		$(".color-code").css("text-align", "left");
 	}
+	
+	$(".hex input").val("");
+	$(".rgb input").val("");
 	
 	$(".swatch .color").on('click', function(){
 		if(colorFormat == "HEX"){
@@ -149,23 +239,54 @@ function swatches(){
 
 if(getCookie("colorFormat") == "" || getCookie("colorFormat") == "HEX"){
 	colorFormat = "HEX";
-	$(".switch input").attr("checked", false);
-	$("textarea").after('<div class="hex">HEX: # <input type="text" name="hex" placeholder="808080"></div>');
+	$(".switch-panel.format input").attr("checked", false);
+	$("textarea").after('<div class="hex">HEX: # <input type="text" name="hex"></div>');
 }
 else{
 	colorFormat = "RGB";
-	$(".switch input").attr("checked", true);
-	$("textarea").after('<div class="rgb">rgb(<input type="text" name="red" placeholder="128">, <input type="text" name="green" placeholder="128">, <input type="text" name="blue" placeholder="128">)</div>');
+	$(".switch-panel.format input").attr("checked", true);
+	$("textarea").after('<div class="rgb">rgb(<input type="text" name="red">, <input type="text" name="green">, <input type="text" name="blue">)</div>');
 }
 
-$(".switch input").on('click', function(){
+if(getCookie("bezelStyle") == "" || getCookie("bezelStyle") == "koko-aio"){
+	bezelStyle = "koko-aio";
+	$(".switch-panel.bezel input").attr("checked", false);
+	
+	if($("div").hasClass("hex") == true){
+		$(".hex input").attr("placeholder", "808080");
+	}
+	else{
+		$(".rgb input[name='red']").attr("placeholder", "128");
+		$(".rgb input[name='green']").attr("placeholder", "128");
+		$(".rgb input[name='blue']").attr("placeholder", "128");
+	}
+}
+else{
+	bezelStyle = "mbz";
+	$(".switch-panel.bezel input").attr("checked", true);
+	
+	if($("div").hasClass("hex") == true){
+		$(".hex input").attr("placeholder", "1A1A1A");
+	}
+	else{
+		$(".rgb input[name='red']").attr("placeholder", "26");
+		$(".rgb input[name='green']").attr("placeholder", "26");
+		$(".rgb input[name='blue']").attr("placeholder", "26");
+	}
+}
+
+$(".switch-panel.bezel input").on('click', function(){
+	bezelToggle();
+});
+
+$(".switch-panel.format input").on('click', function(){
 	formatToggle();
 });
 
+bezelToggle();
+
 $(document).ready(function () {
 	$("form").submit(function (event) {
-		var contrast = $("#contrasts option:selected").val();
-		
 		$(".message").removeClass("info");
 		$(".message").removeClass("error");
 		$(".message").empty();
@@ -189,22 +310,29 @@ $(document).ready(function () {
 				
 				hexError = "false";
 				
-				if(contrast < 1){
-					rSetting = (((hexToRgb(hex).r - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .1 + .105) + (contrast * .01 + .0105))) / 128)).toFixed(10);
-					gSetting = (((hexToRgb(hex).g - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .1 + .105) + (contrast * .01 + .0105))) / 128)).toFixed(10);
-					bSetting = (((hexToRgb(hex).b - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .01 + .055) + (contrast * .001 + .0055))) / 128)).toFixed(10);
+				if(bezelStyle == "koko-aio"){
+					var contrast = $("#contrasts option:selected").val();
+					if(contrast < 1){
+						rSetting = (((hexToRgb(hex).r - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .1 + .105) + (contrast * .01 + .0105))) / 128)).toFixed(10);
+						gSetting = (((hexToRgb(hex).g - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .1 + .105) + (contrast * .01 + .0105))) / 128)).toFixed(10);
+						bSetting = (((hexToRgb(hex).b - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .01 + .055) + (contrast * .001 + .0055))) / 128)).toFixed(10);
+					}
+					else{
+						rSetting = (((hexToRgb(hex).r - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .01 + .105) + (contrast * .001 + .0105))) / 128)).toFixed(10);
+						gSetting = (((hexToRgb(hex).g - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .01 + .105) + (contrast * .001 + .0105))) / 128)).toFixed(10);
+						bSetting = (((hexToRgb(hex).b - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .001 + .055) + (contrast * .0001 + .0055))) / 128)).toFixed(10);
+					}
+					
+					contrastSetting = contrast+"0000";
+					$(".conversion").text('BEZEL_R = "'+rSetting+'"\nBEZEL_G = "'+gSetting+'"\nBEZEL_B = "'+bSetting+'"\nBEZEL_CON = "'+contrastSetting+'"');
 				}
 				else{
-					rSetting = (((hexToRgb(hex).r - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .01 + .105) + (contrast * .001 + .0105))) / 128)).toFixed(10);
-					gSetting = (((hexToRgb(hex).g - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .01 + .105) + (contrast * .001 + .0105))) / 128)).toFixed(10);
-					bSetting = (((hexToRgb(hex).b - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .001 + .055) + (contrast * .0001 + .0055))) / 128)).toFixed(10);
+					mbzHSB = rgbToHSB(hexToRgb(hex).r, hexToRgb(hex).g, hexToRgb(hex).b);
+					$(".conversion").text('HSM_BZL_COLOR_HUE = "'+Math.round(mbzHSB[0])+'.000000"\nHSM_BZL_COLOR_SATURATION = "'+Math.round(mbzHSB[1])+'.000000"\nHSM_BZL_COLOR_VALUE = "'+Math.round(mbzHSB[2])+'.000000"');
 				}
 				
 				$(".square").css("box-shadow", "0px 0px 14px 4px rgba("+hexToRgb(hex).r+", "+hexToRgb(hex).g+", "+hexToRgb(hex).b+", 0.25)");
 				$(".square").css("background", "#"+hex);
-				
-				contrastSetting = contrast+"0000";
-				$(".conversion").text('BEZEL_R = "'+rSetting+'"\nBEZEL_G = "'+gSetting+'"\nBEZEL_B = "'+bSetting+'"\nBEZEL_CON = "'+contrastSetting+'"')
 				
 				$("#copy").prop("disabled", false);
 				
@@ -236,6 +364,7 @@ $(document).ready(function () {
 						$(".message").addClass("info");
 						break;
 					case "553A99":
+					case "908A99":
 					case "B5B6E4":
 					case "D1D1D1":
 						$(".message").text("Super Nintendo");
@@ -278,23 +407,30 @@ $(document).ready(function () {
 			}
 			else{
 				rgbError = "false";
+				
+				if(bezelStyle == "koko-aio"){
+					var contrast = $("#contrasts option:selected").val();
+					if(contrast < 1){
+						rSetting = (((r - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .1 + .105) + (contrast * .01 + .0105))) / 128)).toFixed(10);
+						gSetting = (((g - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .1 + .105) + (contrast * .01 + .0105))) / 128)).toFixed(10);
+						bSetting = (((b - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .01 + .055) + (contrast * .001 + .0055))) / 128)).toFixed(10);
+					}
+					else{
+						rSetting = (((r - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .01 + .105) + (contrast * .001 + .0105))) / 128)).toFixed(10);
+						gSetting = (((g - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .01 + .105) + (contrast * .001 + .0105))) / 128)).toFixed(10);
+						bSetting = (((b - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .001 + .055) + (contrast * .0001 + .0055))) / 128)).toFixed(10);
+					}
 					
-				if(contrast < 1){
-					rSetting = (((r - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .1 + .105) + (contrast * .01 + .0105))) / 128)).toFixed(10);
-					gSetting = (((g - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .1 + .105) + (contrast * .01 + .0105))) / 128)).toFixed(10);
-					bSetting = (((b - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .01 + .055) + (contrast * .001 + .0055))) / 128)).toFixed(10);
+						contrastSetting = contrast+"0000";
+						$(".conversion").text('BEZEL_R = "'+rSetting+'"\nBEZEL_G = "'+gSetting+'"\nBEZEL_B = "'+bSetting+'"\nBEZEL_CON = "'+contrastSetting+'"');
 				}
 				else{
-					rSetting = (((r - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .01 + .105) + (contrast * .001 + .0105))) / 128)).toFixed(10);
-					gSetting = (((g - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .01 + .105) + (contrast * .001 + .0105))) / 128)).toFixed(10);
-					bSetting = (((b - 128) * (0.395 - (contrast - 1.30) * ((3.75 - contrast) * (contrast * .001 + .055) + (contrast * .0001 + .0055))) / 128)).toFixed(10);
+					mbzHSB = rgbToHSB(r, g, b);
+					$(".conversion").text('HSM_BZL_COLOR_HUE = "'+Math.round(mbzHSB[0])+'.000000"\nHSM_BZL_COLOR_SATURATION = "'+Math.round(mbzHSB[1])+'.000000"\nHSM_BZL_COLOR_VALUE = "'+Math.round(mbzHSB[2])+'.000000"');
 				}
 				
 				$(".square").css("box-shadow", "0px 0px 14px 4px rgba("+r+", "+g+", "+b+", 0.25)");
 				$(".square").css("background", rgb);
-				
-				contrastSetting = contrast+"0000";
-				$(".conversion").text('BEZEL_R = "'+rSetting+'"\nBEZEL_G = "'+gSetting+'"\nBEZEL_B = "'+bSetting+'"\nBEZEL_CON = "'+contrastSetting+'"')
 				
 				$("#copy").prop("disabled", false);
 				
@@ -326,6 +462,7 @@ $(document).ready(function () {
 						$(".message").addClass("info");
 						break;
 					case "rgb(85, 58, 153)":
+					case "rgb(144,138,153)":
 					case "rgb(181, 182, 228)":
 					case "rgb(209, 209, 209)":
 						$(".message").text("Super Nintendo");
@@ -355,6 +492,4 @@ $(document).ready(function () {
 		event.preventDefault();
 		
 	});
-	
-	swatches();
 });
