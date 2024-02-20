3d8113208c677c7a000267965a5499414f781796
hgoda90 <hgoda90@gmail.com>
2024-02-19 21:56:21 -0500

    add css style for empty tabs

--- assets/functions.js
+++ assets/functions.js
@@ -61,6 +61,7 @@ function clearText(id){
 	else{
 		$(".text-box .tab-pane.active textarea").text("").val("");
 		$(".preset-title.show").empty();
+		$("#tab"+(id-1)).parents(".nav-item").addClass("empty");
 	}
 }
 
@@ -223,6 +224,7 @@ function copy(id){
 function destroy(){
 	$(".preset-title").empty();
 	$("textarea").val("").text("");
+	$(".nav-item").addClass("empty");
 	colorReset();
 }
 
@@ -1126,18 +1128,23 @@ dropText2.ondrop = function(e) {
 					$(".nav.nav-tabs").append('<li class="nav-item" role="presentation"><button class="nav-link" id="tab'+(tabs+(i+1))+'" data-bs-toggle="tab" data-bs-target="#tab-pane'+(tabs+(i+1))+'" type="button" role="tab" aria-controls="tab-pane'+(tabs+(i+1))+'" aria-selected="true">'+(tabs+(i+1))+'</button></li>');
 				}
 			}
+			else{
+				$(".nav-tabs #tab"+(e.dataTransfer.files.length+1)).parents(".nav-item").prev().removeClass("empty");
+			}
 	   }
   }
   else{
 	  for (var i=0; i<12;i++){
 			files[i] = e.dataTransfer.files[i];
 			dropfile2(files);
+			$(".nav-item").removeClass("empty");
 			if($(".nav").children().length < 12){
 				if(i == 0 && $(".nav").children().length == 0){
 					$(".nav.nav-tabs").append('<li class="nav-item" role="presentation"><button class="nav-link active" id="tab1" data-bs-toggle="tab" data-bs-target="#tab-pane1" type="button" role="tab" aria-controls="tab-pane1" aria-selected="true">1</button></li>');
 				}
 				else{
 					$(".nav.nav-tabs").append('<li class="nav-item" role="presentation"><button class="nav-link" id="tab'+(i+1)+'" data-bs-toggle="tab" data-bs-target="#tab-pane'+(i+1)+'" type="button" role="tab" aria-controls="tab-pane'+(i+1)+'" aria-selected="true">'+(i+1)+'</button></li>');
+					$("#tab"+(i+1)).parents(".nav-item").removeClass("empty");
 				}
 			}
 	   }
